47488e7ba0509fae6f0c666218f979cfb58e8778
alambike <alambike@alambike-MM061.(none)>
2013-02-27 01:31:03 +0100

    - nginx new source version (1.2.7)
    	modified:   attributes/default.rb
    	modified:   recipes/default.rb

--- cookbooks/appserver_nginx/attributes/default.rb
+++ cookbooks/appserver_nginx/attributes/default.rb
@@ -1,10 +1,10 @@
-default["appserver"]["nginx"]["version"] = "1.2.7"
+#default["appserver"]["nginx"]["version"] = "1.2.6"
 #default["appserver"]["nginx"]["checksum"] = "0510af71adac4b90484ac8caf3b8bd519a0f7126250c2799554d7a751a2db388"
-default["appserver"]["nginx"]["checksum"] = "2457a878943fb409ec4fcb46b43af222d06a584f93228e17a4f02b0e7bfc9de3"
 
+default["appserver"]["nginx"]["version"] = "1.2.7"
+default["appserver"]["nginx"]["checksum"] = "2457a878943fb409ec4fcb46b43af222d06a584f93228e17a4f02b0e7bfc9de3"
 
 default["appserver"]["nginx"]["modules"] = [
     "http_ssl_module",
     "http_gzip_static_module"
 ]
-
--- cookbooks/appserver_nginx/recipes/default.rb
+++ cookbooks/appserver_nginx/recipes/default.rb
@@ -4,14 +4,29 @@ node.default["appserver"]["version"] = node["appserver"]["nginx"]["version"]
 
 ## instalamos nginx, ultima version disponible
 node.set["nginx"]["version"] = node["appserver"]["nginx"]["version"]
-#node.set['nginx']['source']['url'] = "http://nginx.org/download/nginx-1.2.7.tar.gz"
+node.set["nginx"]['source']["version"] = node["appserver"]["nginx"]["version"]
 node.set['nginx']['source']['checksum'] = node["appserver"]["nginx"]["checksum"]
+
+## seteamos os atributos compostos, de igual forma que se fai no attributes/source.rb de nginx
+# para establecer os valores correctos en funcion da version escollida
+node.set['nginx']['source']['url']     = "http://nginx.org/download/nginx-#{node['nginx']['source']['version']}.tar.gz"
+
+node.set['nginx']['source']['prefix'] = "/opt/nginx"
+#node.set['nginx']['source']['prefix'] = "/opt/nginx-#{node['nginx']['source']['version']}"
+#
+node.set['nginx']['source']['conf_path'] = "#{node['nginx']['dir']}/nginx.conf"
+node.set['nginx']['source']['default_configure_flags'] = [
+  "--prefix=#{node['nginx']['source']['prefix']}",
+  "--conf-path=#{node['nginx']['dir']}/nginx.conf"
+]
 
 ## seteamos os parametros para a receta de nginx da comunidade
 node.set["nginx"]["source"]["modules"] = node["appserver"]["nginx"]["modules"]
 
 Chef::Log.info("APPSERVER_ID: #{node['appserver']['id']}")
 Chef::Log.info("APPSERVER_VERSION: #{node["appserver"]["version"]}")
-Chef::Log.info("nginx_VERSION: #{node["appserver"]["nginx"]["version"]}")
+
+require 'pp'
+pp node.debug_value(:nginx)
 
 include_recipe "nginx::source"
