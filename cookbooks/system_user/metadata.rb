fb67112ed6b4ff14943dd461423850bb91afd045
J. Gomez <alambike@gmail.com>
2013-05-29 20:04:07 +0200

    deleted:    README.md 	modified:   metadata.rb

--- cookbooks/system_user/metadata.rb
+++ cookbooks/system_user/metadata.rb
@@ -2,7 +2,7 @@ name             'system_user'
 maintainer       'RIYIC'
 maintainer_email 'info@riyic.com'
 license          'Apache 2.0'
-description      'Installs/Configures system_user'
+description      'Create/update a system user with a associated shell'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
