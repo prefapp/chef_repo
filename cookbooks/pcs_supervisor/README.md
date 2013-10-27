f4a6751f6f932656a3427835745f739bc252cdbb
J. Gomez <alambike@gmail.com>
2013-10-28 00:28:46 +0100

    Initial commit 	new file:   README.md 	new file:   metadata.rb 	new file:   recipes/default.rb 	new file:   templates/default/supervisord.conf.erb

---
+++ cookbooks/pcs_supervisor/README.md
@@ -0,0 +1,6 @@
+appserver_nginx
+===============
+
+Nginx cookbook.
+
+Depends on nginx community cookbook (https://github.com/opscode-cookbooks/nginx)
---
+++ cookbooks/pcs_supervisor/metadata.rb
@@ -0,0 +1,24 @@
+name             "pcs_supervisor"
+maintainer       "RIYIC"
+maintainer_email "info@riyic.com"
+license          "Apache 2.0"
+version          "0.1.0"
+description      "Install/Configures supervisor process control system"
+
+%w{debian ubuntu}.each do |os|
+  supports os
+end
+
+depends "supervisor"
+
+recipe "default",
+    :description => "Installs supervisor and python support",
+    :attributes => [/.+/]
+
+    
+#attribute "appserver/nginx/passenger/version",
+#    :display_name => 'Passenger version',
+#    :description => 'Passenger version to compile and install with nginx',
+#    :default => '4.0.8', #'3.0.21',
+#    :advanced => false,
+#    :validations => {predefined: "version"}
