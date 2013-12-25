4283bb874b4795f15fe9d6774b4a9775162d462a
J. Gomez <alambike@gmail.com>
2013-12-25 13:14:26 +0100

    Test if array attributes exists before iterate
    	modified:   recipes/psgi_app.rb

--- cookbooks/app_perl/recipes/psgi_app.rb
+++ cookbooks/app_perl/recipes/psgi_app.rb
@@ -42,55 +42,56 @@ node["app"]["perl"]["psgi_apps"].each do |app|
   }
 
   # instalamos a librerias que necesite a applicacion
-  # rvm_shell "bundle install" do    
-  #     user        app["owner"]
-  #     group       app["group"]
-  #     cwd         app["target_path"]
-  #     environment env_hash
-  #     ## fundamental meter o --path dentro do propio deploy, senon trata de instalar no home do usuario que lance o sudo
-  #     code        %{bundle install --path=vendor/bundle --binstubs --without #{app["exclude_bundler_groups"].join(' ')}}
-  # end
+  if app["extra_modules"] and app["extra_modules"].size > 0 
+    perlenv_cpanm 'install_extra_modules' do
+        modules app["extra_modules"]
+    end
+  end
 
 
   # aplicamos a migracion
-  # if app["migrate"] == "yes"
-
-  #   rvm_shell "rake db:migrate" do    
-  #           user        app["owner"]
-  #           group       app["group"]
-  #           cwd         "#{app["target_path"]}"
-  #           environment env_hash
-  #           code        "bundle exec rake db:migrate"
-  #   end
-
-  # end
+  if app["migrate"] == "yes"
+    perlenv_shell "migration_run" do
+        user        app["owner"]
+        group       app["group"]
+        cwd         app["target_path"]
+        environment env_hash
+        command     app["migration_command"]
+    end
+  end
 
 
   # executamos script post deploy
   if app["postdeploy_script"]
-    # rvm_shell "postdeploy" do    
-    #     user        app["owner"]
-    #     group       app["group"]
-    #     cwd         app["target_path"]
-    #     environment env_hash
-    #     code        %{bash #{app["target_path"]}/#{app["postdeploy_script"]}}
-    # end
-    
-    # co -l -c NON FAI FALTA meter o bash nun rvm_shell, pero ambos funcionan perfectamente
-    bash "postdeploy" do    
+
+    perlenv_shell "postdeploy" do    
         user        app["owner"]
         group       app["group"]
         cwd         app["target_path"]
-        code        %{bash -l -c #{app["target_path"]}/#{app["postdeploy_script"]}}
+        code        %{bash #{app["target_path"]}/#{app["postdeploy_script"]}}
     end
 
   end
 
 
-  # configuramos o frontend + backend
-  nginx_psgi_site app["domain"] do
-    static_files_path "#{app["target_path"]}/public"
-    # service_location "apatrullando"
+  # socket que usaremos para comunicar nginx con uwsgi
+  mi_socket = "unix:///tmp/#{app["domain"]}.sock"
+
+  # configuramos o backend
+  uwsgi_psgi_site app["domain"] do
+    app_dir       app["target_path"]
+    entry_point   app["entry_point"] 
+    socket        mi_socket
+
+    # lanzamos a applicacion con 100 coroutines, 
+    coroae        100
+  end
+
+  # configuramos o frontend
+  nginx_uwsgi_site app["domain"] do
+    static_files_path   "#{app["target_path"]}/public"
+    uwsgi_socket        mi_socket
+    protocol            'perl'
   end
 
 end
