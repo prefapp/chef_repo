61565997a443685eb0913a2535a7e3f4b91e558f
J. Gomez <alambike@gmail.com>
2014-01-07 08:13:47 +0100

    Added support for alternate server names in a passenger site
    	modified:   definitions/nginx_passenger_site.rb
    	modified:   templates/default/passenger_site.erb

--- cookbooks/appserver_nginx/definitions/nginx_passenger_site.rb
+++ cookbooks/appserver_nginx/definitions/nginx_passenger_site.rb
@@ -6,7 +6,7 @@ define :nginx_passenger_site, :template => 'passenger_site.erb', :enable => true
   port = params[:port] || 80
   service_location = params[:service_location] || '/'
   rack_env = params[:rack_env] || 'production'
-  alternative_names = params[:alternative_names] || []
+  alternate_names = params[:server_alias] || []
 
   # nos aseguramos de que se incluan as recetas basicas
   include_recipe "appserver_nginx::with_passenger"
@@ -25,7 +25,7 @@ define :nginx_passenger_site, :template => 'passenger_site.erb', :enable => true
       :static_files_path => static_files_path,
       :service_location  => service_location,
       :rails_env         => rack_env,
-      :alternative_names => alternative_names
+      :alternate_names   => alternate_names
     )
 
   end
