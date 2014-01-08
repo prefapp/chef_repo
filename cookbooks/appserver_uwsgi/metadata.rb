b6b4243e69905f8492b3a16adb9fd6444dd19a38
J. Gomez <alambike@gmail.com>
2014-01-08 03:40:30 +0100

    Bump version to 0.1.1

--- cookbooks/appserver_uwsgi/metadata.rb
+++ cookbooks/appserver_uwsgi/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to install uWSGI and extensions"
-version          "0.0.1"
+version          "0.1.1"
 
 depends "lang_python"
 depends "lang_perl"
