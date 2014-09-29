43bb7f956bbe67f148cc2ca0cf0374172b16a72d
J. Gomez <alambike@gmail.com>
2014-09-30 01:24:50 +0200

    Added python support with a python recipe and a uwsgi_python_site definition

--- cookbooks/appserver_uwsgi/recipes/python.rb
+++ cookbooks/appserver_uwsgi/recipes/python.rb
@@ -1,8 +1,19 @@
+# montamos a base de uwsg
+include_recipe "appserver_uwsgi::default"
+
 # librerias de desarrollo de python (debian, ubuntu)
 package "python-dev"
 
-node.run_state['uwsgi_plugins'] =
-    node.run_state['uwsgi_plugins'] | ['python']
+target_path = node["appserver"]["uwsgi"]["installation_path"]
 
-node.run_state['uwsgi_builds'] =
-    node.run_state['uwsgi_builds'] | ['base']
+
+['python'].each do |p|
+    bash "compile_#{p}" do
+        cwd target_path
+        code "python uwsgiconfig.py --plugin plugins/#{p} core"
+        not_if{File.exists?("#{target_path}/#{p}_plugin.so")}
+    end
+end
+
+# configuracion final
+include_recipe "appserver_uwsgi::_final_config"
