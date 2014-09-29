e6890d5a66250878093e1054cdaad6fd8815de36
J. Gomez <alambike@gmail.com>
2014-09-30 01:23:15 +0200

    Cosmetic changes in passenger recipe

--- cookbooks/appserver_nginx/recipes/with_passenger.rb
+++ cookbooks/appserver_nginx/recipes/with_passenger.rb
@@ -10,15 +10,17 @@ node.set["nginx"]["passenger"]["version"] = node["appserver"]["nginx"]["passenge
 node.set["nginx"]["passenger"]["root"] = "#{node['lang']['ruby']['gemdir']}/gems/passenger-#{node['appserver']['version']}"
 node.set["nginx"]["passenger"]["ruby"] = node['lang']['ruby']['wrapper']
 
-node.set["appserver"]["nginx"]["modules"] = 
-    node["appserver"]["nginx"]["modules"] | ["passenger"]
-
 ## usamos gem_dir e ruby_path, 2 helpers definidos no cookbook lang_ruby
 #node.set["nginx"]["passenger"]["root"] = "#{gem_dir}/gems/passenger-#{node['appserver']['version']}"
 #node.set["nginx"]["passenger"]["ruby"] = ruby_wrapper
 
 Chef::Log.info("NGINX::PASSENGER_ROOT: #{node['nginx']['passenger']['root']} ")
 Chef::Log.info("NGINX::PASSENGER_RUBY: #{node["nginx"]["passenger"]["ruby"]}")
+
+
+# agregamos a instalacion do modulo passenger
+node.set["appserver"]["nginx"]["modules"] = 
+    node["appserver"]["nginx"]["modules"] | ["passenger"]
 
 
 # finalmente incluimos a receta default que vai a compilar o nginx cos modulos agregados
