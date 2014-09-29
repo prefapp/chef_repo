ebb63879061dc51ab2276bf31d0193158e6e7df8
J. Gomez <alambike@gmail.com>
2014-09-30 01:07:50 +0200

    Initial commit

---
+++ cookbooks/app_python/metadata.rb
@@ -0,0 +1,146 @@
+name             "app_python"
+maintainer       "RIYIC"
+maintainer_email "info@riyic.com"
+license          "Apache 2.0"
+description      "Cookbook to deploy python wsgi applications"
+#long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
+
+depends "lang_python"
+depends "appserver_nginx"
+depends "appserver_uwsgi"
+depends "code_repo"
+
+depends "build-essential"
+
+%w{debian ubuntu}.each do |os|
+  supports os
+end
+
+recipe "default",
+    description: "empty",
+    attributes: []
+
+
+recipe "wsgi_app",
+    description: "Deploy a wsgi app from repository with nginx+uwsgi support",
+    attributes: [/^app\/python\/wsgi_apps\//],
+    dependencies: ["lang_python::default", "appserver_nginx::default", "appserver_uwsgi::python"],
+    stackable: true
+
+attribute "app/python/wsgi_apps/@/domain",
+    :display_name => 'Application domain',
+    :description => 'Domain associated to app virtual host',
+    :default => 'test.com',
+    :advanced => false,
+    :required => true,
+    :validations => {predefined: "domain"}
+
+#attribute "app/python/psgi_apps/@/environment",
+#    :display_name => 'Application environment',
+#    :description => 'Application Environment',
+#    :default => 'production',
+#    :advanced => false,
+#    :required => true,
+#    :validations => {predefined: "word"}
+
+attribute "app/python/wsgi_apps/@/target_path",
+    :display_name => "Application deployment folder",
+    :description => 'The application will be deployed to this folder',
+    :default => '/home/owner/my_app',
+    :advanced => false,
+    :validations => {predefined: "unix_path"}
+
+attribute "app/python/wsgi_apps/@/entry_point",
+    :display_name => "Application entry point script",
+    :description => 'Script to start the application',
+    :default => 'app.wsgi',
+    :advanced => false,
+    :validations => {predefined: "unix_path"}
+
+attribute "app/python/wsgi_apps/@/owner",
+    :display_name => "Deployment owner",
+    :description => 'User that shall own the target path',
+    :default => 'owner',
+    :advanced => false,
+    :validations => {predefined: "username"}
+
+attribute "app/python/wsgi_apps/@/group",
+    :display_name => "Deployment group",
+    :description => 'The group that shall own the target path',
+    :default => 'ownergrp',
+    :validations => {predefined: "username"}
+
+
+attribute "app/python/wsgi_apps/@/repo_url",
+    :display_name => 'Repository source code url',
+    :description => 'Repository url from which to download source code',
+    :advanced => false,
+    :required => true,
+    :default => "http://my-repo-url.com",
+    :validations => {predefined: "url"}
+
+
+attribute "app/python/wsgi_apps/@/repo_type",
+    :display_name => "Repository type",
+    :description => 'Repository type from which to download application code',
+    :default => 'git',
+    :advanced => false,
+    :choice => ["git", "subversion","remote_archive"]
+
+attribute "app/python/wsgi_apps/@/revision",
+    :display_name => 'Application Repository revision',
+    :description => 'Application repository tag/branch/commit/archive_name to download',
+    :default => "HEAD",
+    :validations => {predefined: "revision"}
+
+attribute "app/python/wsgi_apps/@/requirements_file",
+    :display_name => 'Requirements file',
+    :description => 'Specify application requirements in a pip requirements file',
+    :advanced => false,
+    :default => "requirements.txt",
+    :required => false,
+    :validations => {predefined: "unix_path"}
+
+
+attribute "app/python/wsgi_apps/@/migrate",
+    :display_name => 'Apply migrations?',
+    :description => 'If "yes" migrations will be run',
+    :advanced => false,
+    :choice => ["yes","no"],
+    :default => "yes",
+    :required => true
+
+attribute "app/python/wsgi_apps/@/migration_command",
+    :display_name => 'Migration command',
+    :description => 'Command to run to migrate application to current state',
+    :default => "",
+    :validations => {predefined: "unix_command"}
+
+attribute "app/python/wsgi_apps/@/credential",
+    :display_name => 'Repository remote user credential',
+    :description => 'Application repository remote user credential',
+    :field_type => 'textarea',
+    :validations => {predefined: "multiline_text"}
+
+attribute "app/python/wsgi_apps/@/extra_modules",
+    :display_name => 'Extra python pip modules',
+    :description => 'Extra python modules needed by the application',
+    :type => "array",
+    :default => [],
+    :validations => {predefined: "python_package"}
+
+attribute "app/python/wsgi_apps/@/extra_packages",
+    :display_name => 'Extra system packages',
+    :description => 'Extra system packages needed by the application',
+    :type => "array",
+    :default => [],
+    :validations => {predefined: "package_name"}
+
+
+attribute "app/python/wsgi_apps/@/postdeploy_script",
+    :display_name => 'Bash script with extra tasks to run',
+    :description => 'Script with extra tasks to run after deploy (relative path from target_path)',
+    :default => "",
+    :validations => {predefined: "unix_path"}
+
---
+++ cookbooks/app_python/recipes/wsgi_app.rb
@@ -0,0 +1,108 @@
+# Seria para usar de forma apilable, para poder facer o deploy de varias apps
+node["app"]["python"]["wsgi_apps"].each do |app|
+
+  #instalar paquetes de sistema necesarios
+  Array(app["extra_packages"]).each do |pkg|
+    package pkg
+  end
+
+  # descargamos o codigo da app
+  # en funcion do tipo de repositorio
+  include_recipe "code_repo::default"
+  
+  case app["repo_type"]
+  when "git" 
+    provider = Chef::Provider::CodeRepoGit
+
+  when "subversion"
+    provider = Chef::Provider::CodeRepoSvn
+
+  when "remote_archive"
+    provider = Chef::Provider::CodeRepoRemoteArchive
+  else
+    provider = Chef::Provider::CodeRepoGit
+  end
+
+  code_repo app["target_path"] do
+    provider    provider
+    action      "pull"
+    owner       app["owner"]
+    group       app["group"]
+    url         app["repo_url"]
+    revision    app["revision"]
+    credential  app["credential"]
+    depth       1
+  end
+
+
+
+  # variables de entorno
+  env_hash = {}
+
+  # instalamos a librerias que necesite a applicacion
+
+  if app["requirements_file"]
+    bash "requirements" do
+      user        app["owner"]
+      group       app["group"]
+      cwd         app["target_path"]
+      environment env_hash
+      command     %{pip install -r #{app["requirements_file"]}}
+    end
+  end
+
+  # modulos que non estan no requirements
+  Array(app["extra_modules"]).each do |p|
+    (name,version) = p.split('#')
+
+    python_pip name do
+      version version if(version)
+    end
+
+  end
+
+
+  # aplicamos a migracion
+  if app["migrate"] == "yes"
+    bash "migration_run" do
+        user        app["owner"]
+        group       app["group"]
+        cwd         app["target_path"]
+        environment env_hash
+        command     app["migration_command"]
+    end
+  end
+
+
+  # executamos script post deploy
+  if app["postdeploy_script"]
+
+    bash "postdeploy" do    
+        user        app["owner"]
+        group       app["group"]
+        cwd         app["target_path"]
+        code        %{#{app["target_path"]}/#{app["postdeploy_script"]}}
+    end
+
+  end
+
+
+  # socket que usaremos para comunicar nginx con uwsgi
+  mi_socket = "unix:///tmp/#{app["domain"]}.sock"
+
+  # configuramos o backend
+  uwsgi_python_site app["domain"] do
+    app_dir       app["target_path"]
+    entry_point   app["entry_point"] 
+    socket        mi_socket
+
+  end
+
+  # configuramos o frontend
+  nginx_uwsgi_site app["domain"] do
+    static_files_path   "#{app["target_path"]}/public"
+    uwsgi_socket        mi_socket
+    protocol            'python'
+  end
+
+end
