f100cca25abdfe0e6d72029592c451292b4589b5
J. Gomez <alambike@gmail.com>
2014-11-13 09:18:28 +0100

    Agregamos recetas dependientes na default, que antes estaban recollidas na definition de uwsgi_site

--- cookbooks/appserver_uwsgi/recipes/default.rb
+++ cookbooks/appserver_uwsgi/recipes/default.rb
@@ -1,4 +1,10 @@
+# requisitos para todas as recetas (psgi, python...)
+# e para os providers
 include_recipe "lang_python::default"
+
+include_recipe "pcs_supervisor::default"
+
+include_recipe "code_repo::default"
 
 include_recipe "build-essential::default"
 
