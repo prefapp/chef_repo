220a488aee15ea7511b9f1dddebf8dbb96f02eb0
J. Gomez <alambike@gmail.com>
2014-11-15 14:09:45 +0100

    Quitamos a instalacion de supervisord porque en ubuntu14 se pode usar upstart ¿?

--- cookbooks/dbs_mysql/recipes/server.rb
+++ cookbooks/dbs_mysql/recipes/server.rb
@@ -20,32 +20,36 @@ node.set["mysql"]["bind_address"] = node["dbs"]["mysql"]["server"]["bind_address
 # incluimos a receta de instalacion do mysql server
 include_recipe "mysql::server"
 
-if node["riyic"]["dockerized"] == "yes"
+#
+# Na imaxen de docker de ubuntu 14.04 funciona o upstart!!!!
+#
 
-    # evitamos cascar no script que setea os grants para debian
-    # porque non vai a estar correndo o servicio
-    ex = resources(execute: "install-grants")
-    ex.command "/bin/true"
+# if node["riyic"]["dockerized"] == "yes"
 
-    # evitamos arrancar o servicio co upstart
-    srv = resources(service: "mysql")
-    srv.start_command "/bin/true"
-    srv.stop_command "/bin/true"
-    srv.restart_command "/bin/true"
-    srv.reload_command "/bin/true"
-    srv.action :nothing
+#     # evitamos cascar no script que setea os grants para debian
+#     # porque non vai a estar correndo o servicio
+#     ex = resources(execute: "install-grants")
+#     ex.command "/bin/true"
 
-    include_recipe "pcs_supervisor::default"
+#     # evitamos arrancar o servicio co upstart
+#     srv = resources(service: "mysql")
+#     srv.start_command "/bin/true"
+#     srv.stop_command "/bin/true"
+#     srv.restart_command "/bin/true"
+#     srv.reload_command "/bin/true"
+#     srv.action :nothing
 
-    supervisor_service "mysqld" do        
-        stdout_logfile "/var/log/supervisor/%(program_name)s.log"
-        stderr_logfile "/var/log/supervisor/%(program_name)s.err"
-        # command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/bin/mysqld_safe --pid-file=/var/run/mysqld/mysqld.pid"
-        command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid"
-        startsecs 2
-        stopsignal "QUIT"
-        stopasgroup true
-        killasgroup true
-        action [:enable,:start]
-    end
-end
+#     include_recipe "pcs_supervisor::default"
+
+#     supervisor_service "mysqld" do        
+#         stdout_logfile "/var/log/supervisor/%(program_name)s.log"
+#         stderr_logfile "/var/log/supervisor/%(program_name)s.err"
+#         # command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/bin/mysqld_safe --pid-file=/var/run/mysqld/mysqld.pid"
+#         command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid"
+#         startsecs 2
+#         stopsignal "QUIT"
+#         stopasgroup true
+#         killasgroup true
+#         action [:enable,:start]
+#     end
+# end
