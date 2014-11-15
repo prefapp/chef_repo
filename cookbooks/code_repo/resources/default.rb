2ae5333538852bb2dc0dc2fa4f6915aca28c9367
J. Gomez <alambike@gmail.com>
2014-11-15 03:32:22 +0100

    Facemos o atibuto url necesario agregado o atributo purge_target_path para permitir o borrado do directorio de clonado, para evitar un fetch

--- cookbooks/code_repo/resources/default.rb
+++ cookbooks/code_repo/resources/default.rb
@@ -2,13 +2,15 @@ actions :pull, :force_pull
 default_action :pull
 
 attribute :target_path,  :kind_of => String, :name_attribute => true
-attribute :url,          :kind_of => String
+attribute :url,          :kind_of => String, :required => true
 attribute :revision,     :kind_of => String
 attribute :owner,        :kind_of => String, :default => "root"
 attribute :group,        :kind_of => String, :default => "root"
 attribute :ssh_host_key, :kind_of => String
 attribute :depth,        :kind_of => Integer
 attribute :credential,   :kind_of => String
+# si purge_target_path = true se borra o target_path antes de facer o pull
+attribute :purge_target_path,   :equal_to => [true,false], :default => false
 
 # atributos concretos para determinados providers
 attribute :unpack_file,  :equal_to => [true,false], :default => true
