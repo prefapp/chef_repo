8ed3a7cf71288f7e4ae7c8e78d6c797e495aae3c
J. Gomez <alambike@gmail.com>
2014-12-11 03:23:51 +0100

    Agregados novo HWRP nginx_fpm_site, e a sua plantilla asociada

---
+++ cookbooks/appserver_nginx/libraries/nginx_fpm_site_resource.rb
@@ -0,0 +1,22 @@
+class Chef
+    class Resource
+        class NginxFpmSite < Chef::Resource::NginxBaseResource
+
+            self.resource_name = 'nginx_fpm_site'
+
+            attribute :document_root,
+                :kind_of => String,
+                :default => ''
+
+            attribute :fpm_socket,
+                :kind_of => String,
+                :default => 'unix:///var/run/php5-fpm.sock'
+
+            attribute :fastcgi_read_timeout,
+                :kind_of => String,
+                :default => "60"
+
+        end
+    end
+end
+ 
