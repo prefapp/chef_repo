212aa3b2608e8a328b9b56d29b96d5b5d981a855
J. Gomez <alambike@gmail.com>
2014-12-11 02:56:17 +0100

    Add default app attributes

--- cookbooks/app_php/attributes/default.rb
+++ cookbooks/app_php/attributes/default.rb
@@ -2,4 +2,23 @@
 # Atributos por defecto app_php
 ##
 
+default["app"]["php"]["default_server_alias"] = []
+default["app"]["php"]["default_target_path"] = nil
+default["app"]["php"]["default_entry_point"] = nil
+default["app"]["php"]["default_owner"] = nil
+default["app"]["php"]["default_group"] = 'users'
 
+default["app"]["php"]["default_repo_url"] = nil
+default["app"]["php"]["default_repo_type"] = 'git'
+default["app"]["php"]["default_revision"] = 'HEAD'
+default["app"]["php"]["default_credential"] = nil
+
+default["app"]["php"]["default_environment"] = 'production'
+default["app"]["php"]["default_static_files_path"] = nil
+default["app"]["php"]["default_migration_command"] = nil
+default["app"]["php"]["default_internal_socket"] = nil
+default["app"]["php"]["default_timeout"] = '60'
+default["app"]["php"]["default_extra_packages"] = []
+default["app"]["php"]["default_purge_target_path"] = 'no'
+default["app"]["php"]["default_purge_repo_depth"] = nil
+default["app"]["php"]["default_extra_modules"] = []
