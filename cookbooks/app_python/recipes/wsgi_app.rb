61c9e68e324facc095d32293623b0e09c513e7f2
J. Gomez <alambike@gmail.com>
2014-12-11 03:19:06 +0100

    Refactorizada receta de wsgi_app para que tire do provider 'wsgi_app' Establecidos valores por defecto dos atributos da receta wsgi_app

--- cookbooks/app_python/recipes/wsgi_app.rb
+++ cookbooks/app_python/recipes/wsgi_app.rb
@@ -1,129 +1,47 @@
+include_recipe "app_python::default"
+
 # Seria para usar de forma apilable, para poder facer o deploy de varias apps
 node["app"]["python"]["wsgi_apps"].each do |app|
 
-  #instalar paquetes de sistema necesarios
-  Array(app["extra_packages"]).each do |pkg|
+	wsgi_app app["domain"] do 
 
-    package pkg do 
-        action :nothing
-    end.run_action(:install)
+                %W(
+                        server_alias 
+                        target_path
+                        entry_point
+                        owner
+                        group
 
-  end
+                        repo_url
+                        repo_type
+                        revision
+                        credential
 
-  # descargamos o codigo da app
-  # en funcion do tipo de repositorio
-  include_recipe "code_repo::default"
-  
-  case app["repo_type"]
-  when "git" 
-    provider = Chef::Provider::CodeRepoGit
+                        environment
 
-  when "subversion"
-    provider = Chef::Provider::CodeRepoSvn
+                        static_files_path
+ 
+                        migration_command
+                        postdeploy_script
 
-  when "remote_archive"
-    provider = Chef::Provider::CodeRepoRemoteArchive
-  else
-    provider = Chef::Provider::CodeRepoGit
-  end
+                        timeout
+                        extra_modules
+                        extra_packages
+                        requirements_file
 
-  code_repo app["target_path"] do
-    provider    provider
-    action      "pull"
-    owner       app["owner"]
-    group       app["group"]
-    url         app["repo_url"]
-    revision    app["revision"]
-    credential  app["credential"]
-    depth       1
-  end
+                        purge_target_path
+                        repo_depth
 
 
+                ).each do |m|
 
-  # variables de entorno
-  env_hash = {}
+                        v = app[m] || node["app"]["wsgi"]["default_#{m}"]
 
-  # instalamos a librerias que necesite a applicacion
+                        self.send(m,v)
 
-  if app["requirements_file"]
+                end
 
-    bash "requirements" do
-      ##########################################################
-      # instalamos como root
-      # para instalar como usuario:
-      # 1) necesitamos que usuario tenha shell e home
-      # 2) necesitamos pasarlle o parametro --user a pip
+                notifies   :restart, 'service[nginx]'
 
-      # user        app["owner"]
-      # group       app["group"]
-      #########################################################
-
-      cwd         app["target_path"]
-      environment env_hash
-      code        %{pip install -r #{app["requirements_file"]}}
-    end
-  end
-
-  # modulos que non estan no requirements
-  Array(app["extra_modules"]).each do |p|
-    (name,version) = p.split('#')
-
-    python_pip name do
-      version version if(version)
-    end
-
-  end
-
-
-  # aplicamos a migracion
-  if app["migrate"] == "yes"
-    bash "migration_run" do
-        user        app["owner"]
-        group       app["group"]
-        cwd         app["target_path"]
-        environment env_hash
-        code        app["migration_command"]
-    end
-  end
-
-
-  # executamos script post deploy
-  if app["postdeploy_script"]
-
-    bash "postdeploy" do    
-        user        app["owner"]
-        group       app["group"]
-        cwd         app["target_path"]
-        code        %{#{app["target_path"]}/#{app["postdeploy_script"]}}
-    end
-
-  end
-
-
-  # socket que usaremos para comunicar nginx con uwsgi
-  mi_socket = "unix:///tmp/#{app["domain"]}.sock"
-
-  # timeout das peticions
-  timeout = app['timeout'] || 120
-
-  # configuramos o backend
-  uwsgi_python_site app["domain"] do
-    app_dir       app["target_path"]
-    entry_point   app["entry_point"] 
-    socket        mi_socket
-    uid           app["owner"]
-    gid           app["group"]
-    options       :harakiri => timeout
-
-  end
-
-  # configuramos o frontend
-  nginx_uwsgi_site app["domain"] do
-    #static_files_path   "#{app["target_path"]}/public"
-    static_files_path   "#{app["target_path"]}/#{app["static_files_path"]}" if app["static_files_path"]
-    uwsgi_socket        mi_socket
-    protocol            'python'
-    uwsgi_read_timeout  timeout
-  end
-
+	end
 end
