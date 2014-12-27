143bc0548781d0c7865c5244475ab41e2aad3511
J. Gomez <alambike@gmail.com>
2014-12-27 12:24:51 +0100

    Bump to 0.1.0

--- cookbooks/appserver_fpm/metadata.rb
+++ cookbooks/appserver_fpm/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to install and configure php-fpm service"
-version          "0.0.1"
+version          "0.1.0"
 
 #depends "php-fpm"
 depends "php5-fpm"
