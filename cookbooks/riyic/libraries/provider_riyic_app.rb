10d9a32050ade6e41f9aca565ae60b73e7e0257c
J. Gomez <alambike@gmail.com>
2015-01-11 04:40:34 +0100

    Update base app provider to support rack_apps

--- cookbooks/riyic/libraries/provider_riyic_app.rb
+++ cookbooks/riyic/libraries/provider_riyic_app.rb
@@ -40,6 +40,10 @@ class Chef
 
             protected
 
+            def env_hash
+                {}
+            end
+
             def install_extra_packages
                 new_resource.extra_packages.each do |pkg|
 
@@ -79,6 +83,16 @@ class Chef
                   credential        new_resource.credential
                   depth             new_resource.repo_depth
                   purge_target_path new_resource.purge_target_path
+                  notifies          :create, "directory[#{new_resource.target_path}/tmp]"
+                end
+
+                # en principio esto so e necesario nas apps rack
+                directory "#{new_resource.target_path}/tmp" do
+                    owner   new_resource.owner
+                    group   new_resource.group
+                    action  :nothing
+                    only_if {new_resource.resource_name == 'rack_app'}
+
                 end
 
             end
@@ -107,7 +121,13 @@ class Chef
                     user        new_resource.owner
                     group       new_resource.group
                     cwd         new_resource.target_path
-                    code        %{#{new_resource.target_path}/#{new_resource.postdeploy_script}}
+                    environment env_hash
+                    code        %{bash #{new_resource.target_path}/#{new_resource.postdeploy_script}}
+
+                    only_if {
+                                !node["riyic"]["inside_container"] &&
+                                ::File.exists?("#{new_resource.target_path}/#{new_resource.postdeploy_script}")
+                    }
                 end
             end
  
