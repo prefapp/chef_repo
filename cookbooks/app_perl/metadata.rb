9b2281ff321648852e5e7fee0eb4efcf5763a7e3
J. Gomez <alambike@gmail.com>
2015-01-13 00:48:54 +0100

    Saltamos a v0.2.1

--- cookbooks/app_perl/metadata.rb
+++ cookbooks/app_perl/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to deploy perl psgi applications"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.1"
 
 depends "lang_perl"
 depends "appserver_nginx"
