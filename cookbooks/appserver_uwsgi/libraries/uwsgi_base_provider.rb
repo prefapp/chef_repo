d444fe69bfdc58b9ca70992233b27ee6d1d5ca58
J. Gomez <alambike@gmail.com>
2015-01-13 00:50:07 +0100

    Agregamos o flag 'die-on-term' para que runit consiga deter a uwsgi Limpiamos comentarios vellos

--- cookbooks/appserver_uwsgi/libraries/uwsgi_base_provider.rb
+++ cookbooks/appserver_uwsgi/libraries/uwsgi_base_provider.rb
@@ -21,8 +21,6 @@ class Chef
                     run_options = {}
                     run_options["socket"] = uwsgi_socket
                     run_options["plugins-dir"] = node["appserver"]["uwsgi"]["installation_path"]
-                    #run_options["plugin"] = "python"
-                    #run_options["wsgi-file"] = new_resource.entry_point
 
                     run_options["processes"] = new_resource.processes if new_resource.processes > 0
                     run_options["threads"] = new_resource.threads if new_resource.threads > 0
@@ -30,6 +28,11 @@ class Chef
                     run_options["chdir"] = new_resource.app_dir
 
                     run_options["master"] = ""
+                    
+                    # necesario para que runit consiga parar el servicio 
+                    # (invierte el comportamiento de uwsgi con SIGKILL y SIGTERM)
+                    run_options["die-on-term"] = ""
+
                     # run_options["daemonize"] = params[:log_file]
 
                     run_options["uid"] = new_resource.uid
@@ -55,26 +58,12 @@ class Chef
 
                     Chef::Log.info("Compilation command: #{command}")
 
+                    # comando para runit (dentro dun docker container)
                     node.set["container_service"]["uwsgi"]["command"] = command
 
                     service "uwsgi" do
                         action [:enable, :start]
                     end
-
-
-                    # deprecado!!
-                    # instalamos o supervisord e configuramos o control do uwsgi
-
-                    #supervisor_service new_resource.name do        
-                    #    stdout_logfile "/var/log/supervisor/#{new_resource.name}.log"
-                    #    stderr_logfile "/var/log/supervisor/#{new_resource.name}.err"
-                    #    command command
-                    #    startsecs 10
-                    #    stopsignal "QUIT"
-                    #    stopasgroup true
-                    #    killasgroup true
-                    #    action [:enable,:start]
-                    #end
 
                 end
           end
