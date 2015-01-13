afc891fc9e9a3af2c3eb277ccc65dd1f515ff422
J. Gomez <alambike@gmail.com>
2015-01-13 01:41:16 +0100

    Bump to v0.2.1

--- cookbooks/appserver_uwsgi/metadata.rb
+++ cookbooks/appserver_uwsgi/metadata.rb
@@ -3,7 +3,7 @@ maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to install uWSGI and extensions"
-version          "0.1.2"
+version          "0.2.1"
 
 depends "lang_python"
 depends "lang_perl"
