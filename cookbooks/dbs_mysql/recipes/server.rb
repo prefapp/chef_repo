2bc67ff17c80000a4115d997277a87834e747b9d
J. Gomez <alambike@gmail.com>
2015-01-15 15:21:28 +0100

    Add script (run, check) to control mysql service from runit inside a docker container Disable apparmor service in docker_container

--- cookbooks/dbs_mysql/recipes/server.rb
+++ cookbooks/dbs_mysql/recipes/server.rb
@@ -2,7 +2,7 @@
 include_recipe "dbs_mysql::default"
 
 # seteamos os atributos da receta mysql::server
-node["dbs"]["mysql"]["tunable"].each do |attribute, value|
+Hash(node["dbs"]["mysql"]["tunable"]).each do |attribute, value|
     node.set["mysql"]["tunable"][attribute] = value
 end
 
@@ -10,6 +10,7 @@ end
     root_password 
     repl_password 
     debian_password 
+
     }).each do |attribute|
 
     node.set["mysql"]["server_#{attribute}"] = node["dbs"]["mysql"]["server"][attribute]
@@ -17,39 +18,54 @@ end
 
 node.set["mysql"]["bind_address"] = node["dbs"]["mysql"]["server"]["bind_address"]
 
-# incluimos a receta de instalacion do mysql server
+# mysql e un pouco especial para controlar co runit
+# en vez dun command temos que pasarlle o script enteiro
+node.set['container_service']['mysql']['run_script_content'] = <<EOF
+#!/bin/sh
+cd /
+umask 077
+
+MYSQLADMIN='/usr/bin/mysqladmin --defaults-extra-file=/etc/mysql/debian.cnf'
+
+trap "$MYSQLADMIN shutdown" 0
+trap 'exit 2' 1 2 3 15
+
+/usr/bin/mysqld_safe --user=mysql & wait
+
+EOF
+
+# mais un script para chequear que esta levantado
+node.set['container_service']['mysql']['check_script_content'] = <<EOF
+#!/bin/sh
+test -e /var/run/mysqld/mysqld.sock
+EOF
+
+
+# apparmor non se usa en containers pero para poder quitalo temos que facer
+# un monton de cousas
+node.set['container_service']['apparmor']['command'] = "sleep 100"
+node.set['container_service']['apparmor']['disable'] = true
+node.set['container_service']['apparmor-mysql']['command'] = "sleep 100"
+node.set['container_service']['apparmor-mysql']['disable'] = true
+
+## incluimos a receta de instalacion do mysql server
 include_recipe "mysql::server"
 
+# deshabilitamos e paramos o servicio fake apparmor 
+#node.set['container_service']['apparmor-mysql2']['command'] = "sleep 100"
 #
-# Na imaxen de docker de ubuntu 14.04 funciona o upstart!!!!
+#service 'apparmor-mysql2' do
+#    service_name 'apparmor'
+#    action :stop
+#    supports :stop => true
+#end
+
+#node.set['container_service']['default :create mysql']['command'] = 'aa'
+#node.set['container_service']['default :create apparmor']['command'] = 'aa'
 #
+#mysql_service 'no_runit_mysql' do
+#    initial_root_password   node['dbs']['mysql']['server']['root_password']
+#    action                  [:create, :start]
+#    bind_address            node['dbs']['mysql']['server']['bind_address']
+#end
 
-# if node["riyic"]["dockerized"] == "yes"
-
-#     # evitamos cascar no script que setea os grants para debian
-#     # porque non vai a estar correndo o servicio
-#     ex = resources(execute: "install-grants")
-#     ex.command "/bin/true"
-
-#     # evitamos arrancar o servicio co upstart
-#     srv = resources(service: "mysql")
-#     srv.start_command "/bin/true"
-#     srv.stop_command "/bin/true"
-#     srv.restart_command "/bin/true"
-#     srv.reload_command "/bin/true"
-#     srv.action :nothing
-
-#     include_recipe "pcs_supervisor::default"
-
-#     supervisor_service "mysqld" do        
-#         stdout_logfile "/var/log/supervisor/%(program_name)s.log"
-#         stderr_logfile "/var/log/supervisor/%(program_name)s.err"
-#         # command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/bin/mysqld_safe --pid-file=/var/run/mysqld/mysqld.pid"
-#         command "/usr/local/bin/pidproxy /var/run/mysqld/mysqld.pid /usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid"
-#         startsecs 2
-#         stopsignal "QUIT"
-#         stopasgroup true
-#         killasgroup true
-#         action [:enable,:start]
-#     end
-# end
