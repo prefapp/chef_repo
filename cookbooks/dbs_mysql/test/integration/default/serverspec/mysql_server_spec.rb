0405cb3188d423749a5893de7f04e4b24199f60a
J. Gomez <alambike@gmail.com>
2015-01-15 15:21:45 +0100

    Tests with test-kitchen + serverspec

---
+++ cookbooks/dbs_mysql/test/integration/default/serverspec/mysql_server_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "postgresql" do
+
+    it "is listening on port tcp/3306 of 0.0.0.0" do
+        expect(port(3306)).to be_listening.on('0.0.0.0').with('tcp')
+    end
+
+    it "has a running service 'mysqld'" do
+        expect(service("mysqld")).to be_running
+    end
+    
+    db = $node['dbs']['mysql']['dbs'][0]
+    root_pass = $node['dbs']['mysql']['server']['root_password']
+
+    # debe existir a db 
+    describe command("mysql -u root -p#{root_pass} -e 'SHOW DATABASES'") do
+        its(:stdout) {should match /#{db['name']}/}
+    end
+    
+    # debe existir o user_db
+    describe command(
+        "mysql -u root -p#{root_pass} -e 'SELECT * FROM user WHERE User = #{db['user']}' mysql"
+    ) do
+        its(:stdout) {should match /#{db['user']}/}
+    end
+
+end
