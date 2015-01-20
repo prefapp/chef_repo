3b1c0a96aa8ac2937af057510b4549b77df5a29d
J. Gomez <alambike@gmail.com>
2015-01-20 08:51:07 +0100

    Corrected metadata and add new parameters

--- cookbooks/app_phplist/metadata.rb
+++ cookbooks/app_phplist/metadata.rb
@@ -2,13 +2,12 @@ name             "app_phplist"
 maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
-description      "Cookbook to manage odoo (openerp) installations"
+description      "Cookbook to manage phplist (openerp) installations"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
 depends "app_php"
 depends "dbs_mysql"
-depends "code_repo"
 
 %w{debian ubuntu}.each do |os|
   supports os
@@ -35,24 +34,7 @@ attribute "app/phplist/installations/@/alias",
     :validations => {predefined: "domain"}
 
 
-attribute "app/phplist/installations/@/admin_passwd",
-    :display_name => "Phplist admin password",
-    :description => "Phplist admin password",
-    :required => true,
-    :calculated => true,
-    :validations => {predefined: "password"}
-
-
-attribute "app/phplist/installations/@/data_dir",
-    :display_name => "Data directory",
-    :description => "phplist data directory",
-    :required => true,
-    :default => "/opt/odoo/data",
-    :validations => {predefined: "unix_path"}
-
-
-
-attribute "app/odoo/installations/@/db_host",
+attribute "app/phplist/installations/@/db_host",
     :display_name => "Database host",
     :description => "Database host",
     :default => "db",
@@ -60,81 +42,62 @@ attribute "app/odoo/installations/@/db_host",
     :validations => {predefined: "hostname"}
 
 
-attribute "app/odoo/installations/@/db_name",
+attribute "app/phplist/installations/@/db_name",
     :display_name => "Database name",
     :description => "Database Name",
-    :default => "odoo_db",
+    :default => "phplist_db",
     :required => true,
-    :validations => {predefined: "postgresql_identifier"}
+    :validations => {predefined: "mysql_dbname"}
 
-attribute "app/odoo/installations/@/db_user",
+attribute "app/phplist/installations/@/db_user",
     :display_name => "Database username",
     :description => "Database related user",
-    :default => "odoo_user",
+    :default => "phplist_user",
     :required => true,
-    :validations => {predefined: "postgresql_identifier"}
+    :validations => {predefined: "mysql_dbuser"}
 
-attribute "app/odoo/installations/@/db_password",
+attribute "app/phplist/installations/@/db_password",
     :display_name => "Database user password",
     :description => "Database user password",
     :required => true,
     :calculated => true,
-    :validations => {predefined: "db_password"}
+    :validations => {predefined: "mysql_dbpassword"}
 
 
-attribute "app/odoo/installations/@/environment",
-    :display_name => 'Application environment',
-    :description => 'Application Environment',
-    :default => 'production',
-    :required => true,
-    :validations => {predefined: "word"}
-
-attribute "app/odoo/installations/@/target_path",
+attribute "app/phplist/installations/@/target_path",
     :display_name => "Application deployment folder",
     :description => 'The application will be deployed to this folder',
-    :default => '/opt/odoo',
+    :default => '/opt/phplist',
     :validations => {predefined: "unix_path"}
 
-attribute "app/odoo/installations/@/entry_point",
-    :display_name => "Application entry point script",
-    :description => 'Script to start the application',
-    :default => 'openerp-wsgi.py',
-    :validations => {predefined: "unix_path"}
-
-attribute "app/odoo/installations/@/repo_url",
-    :display_name => 'Repository source code url',
-    :description => 'Repository url from which to download source code',
-    :required => true,
-    :default => "http://my-repo-url.com",
-    :validations => {predefined: "url"}
-
-attribute "app/odoo/installations/@/repo_type",
-    :display_name => "Repository type",
-    :description => 'Repository type from which to download application code',
-    :default => 'git',
-    :choice => ["git", "subversion","remote_archive"]
-
-attribute "app/odoo/installations/@/revision",
-    :display_name => 'Application Repository revision',
-    :description => 'Application repository tag/branch/commit/archive_name to download',
-    :default => "8.0",
-    :validations => {predefined: "revision"}
-
-attribute "app/odoo/installations/@/credential",
-    :display_name => 'Repository remote user credential',
-    :description => 'Application repository remote user credential',
-    :field_type => 'textarea',
-    :validations => {predefined: "multiline_text"}
-
-attribute "app/odoo/installations/@/user",
+attribute "app/phplist/installations/@/user",
     :display_name => "Deployment owner",
     :description => 'User that shall own the target path',
-    :default => 'odoo',
+    :default => 'phplist',
     :validations => {predefined: "username"}
 
-attribute "app/odoo/installations/@/group",
+attribute "app/phplist/installations/@/group",
     :display_name => "Deployment group",
     :description => 'The group that shall own the target path',
     :default => 'users',
     :validations => {predefined: "username"}
+
+attribute "app/phplist/installations/@/smtp_server",
+    :display_name => "Smtp host",
+    :description => "Smtp server to use to send emails, empty to localhost",
+    :default => '',
+    :required => false,
+    :validations => {predefined: "hostname"}
+
+attribute "app/phplist/installations/@/smtp_user",
+    :display_name => "Smtp username",
+    :description => "Smtp server login account",
+    :default => '',
+    :required => false,
+    :validations => {predefined: "smtp_user"}
+
+attribute "app/phplist/installations/@/smtp_password",
+    :display_name => "Smtp user password",
+    :description => "Smtp server account password",
+    :validations => {predefined: "password"}
 
