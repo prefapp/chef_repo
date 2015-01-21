686073ed1f8349bfc9a243bfaa3068e8da1dbb11
J. Gomez <alambike@gmail.com>
2015-01-21 02:12:50 +0100

    Bump version to 0.1.3

--- cookbooks/app_phplist/metadata.rb
+++ cookbooks/app_phplist/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to manage phplist installations"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
 depends "app_php"
 depends "dbs_mysql"
