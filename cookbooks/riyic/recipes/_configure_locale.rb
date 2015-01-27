a0aa5180f255251ba2bdb3d8f91a715cffa24d80
J. Gomez <alambike@gmail.com>
2015-01-27 08:50:38 +0100

    Set lc_all attribute when set locales, needed to install other locales instead of en_US.utf8

--- cookbooks/riyic/recipes/_configure_locale.rb
+++ cookbooks/riyic/recipes/_configure_locale.rb
@@ -1,7 +1,9 @@
 # por siacaso
 Encoding.default_external = Encoding::UTF_8 if RUBY_VERSION > "1.9"
 
+# seteamos as locales a instalar
 node.override["locale"]["lang"] = node["riyic"]["system_locale"]
+node.override["locale"]["lc_all"] = node["riyic"]["system_locale"]
 
 include_recipe "locale::default"
 
