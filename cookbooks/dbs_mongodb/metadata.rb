874019f720b4a171ad2a455260ba380d8e90fe2b
J. Gomez <alambike@gmail.com>
2015-02-24 09:01:13 +0100

    Bump to 0.1.4

--- cookbooks/dbs_mongodb/metadata.rb
+++ cookbooks/dbs_mongodb/metadata.rb
@@ -2,7 +2,7 @@ maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Installs/Configures mongodb client/server"
-version          "0.1.2"
+version          "0.1.4"
 name             "dbs_mongodb"
 
 %w{debian ubuntu}.each do |os|
