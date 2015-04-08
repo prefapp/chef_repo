4a936f736416ccc95918067125f288c8edc43cbb
J. Gomez <alambike@gmail.com>
2015-04-09 01:33:29 +0200

    Bump to 0.1.1

--- cookbooks/app_owncloud/metadata.rb
+++ cookbooks/app_owncloud/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "info@riyic.com"
 license          "Apache 2.0"
 description      "Cookbook to manage owncloud"
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 depends "app_php"
 depends "dbs_mysql"
