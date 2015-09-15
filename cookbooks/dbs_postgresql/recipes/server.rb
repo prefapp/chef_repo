8847e91a78737e88dc21245f66349b02d4272331
J. Gomez <alambike@gmail.com>
2015-09-15 09:38:08 +0200

    Para o caso de estarse executando en containers, agregamos como script de extra_tasks, o cambio de propietario do volumen de datos de postgres

--- cookbooks/dbs_postgresql/recipes/server.rb
+++ cookbooks/dbs_postgresql/recipes/server.rb
@@ -20,4 +20,18 @@ EOH
 
 include_recipe "postgresql::server"
 
+# queremos por defecto o charset das dbs que se creen a utf8
 include_recipe "dbs_postgresql::_default_charset_utf8"
+
+
+# extra_tasks para o arranque do container
+if node["riyic"]["inside_container"]
+    
+    file "#{node['riyic']['extra_tasks_dir']}/#{dbs_postgresql-server.sh}" do
+        mode '0700'
+        owner 'root'
+        group 'root'
+        content "chown -R #{postgres}:#{postgres} #{node['postgresql']['data_directory']}"
+    end    
+
+end
