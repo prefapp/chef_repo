143c2040d531c644c412a180ae36e5763838acc7
cechevarne <cechevarne@gmail.com>
2015-10-04 12:14:28 +0200

    Eliminada variable java_home
    Fallaba la receta al hacer un link a /usr/lib/jvm/

--- cookbooks/lang_java/recipes/install.rb
+++ cookbooks/lang_java/recipes/install.rb
@@ -2,6 +2,6 @@
 node.set["java"]["jdk_version"] = node['lang']['java']['jdk_version']
 node.set["java"]["install_flavor"] = node['lang']['java']['install_flavor']
 node.set['java']['oracle']['accept_oracle_download_terms'] = true
-node.set["java"]["java_home"] = '/usr/local/java'
+#node.set["java"]["java_home"] = '/usr/local/java'
 
 include_recipe "java::default"
