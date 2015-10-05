05796617a7a6af488b081ef4ef6467f82c3caa74
J. Gomez <alambike@gmail.com>
2015-10-05 07:38:32 +0200

    Corrections in metadata: Add parameters to recipe Add validations in parameters

--- cookbooks/lang_java/metadata.rb
+++ cookbooks/lang_java/metadata.rb
@@ -2,7 +2,7 @@ name             "lang_java"
 maintainer       "RIYIC"
 maintainer_email "info@riyic.com"
 license          "Apache 2.0"
-description      " This cookbook installs a Java JDK/Oracle. "
+description      " This cookbook installs a Java JDK/JRE. It defaults install OpenJDK but can also install Oracle. "
 #long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
@@ -13,20 +13,26 @@ depends "java"
   supports os
 end
 
-recipe "install", description: "Installs Java"
+recipe "install", 
+    description: "Installs Java JDK",
+    attributes: [/.+/]
+
 
 attribute "lang/java/install_flavor",
-    :display_name => 'Flavor of JVM',
-    :description => 'Flavor of JVM you would like installed',
-    :default => ['openjdk'],
+    :display_name => 'JDK flavor',
+    :description => 'Flavor of JDK you would like to install',
+    :default => 'openjdk',
     :choice => %w{openjdk oracle}
    
 attribute "lang/java/jdk_version",
     :display_name => 'JDK version',
-    :description => 'JDK version to install'
+    :description => 'JDK version to install',
+    :default => '8',
+    :choice => %w{6 7 8}
   
 attribute "lang/java/java_home",
     :display_name => 'JVM install dir',
-    :description => 'JVM install dir'
+    :description => 'JVM install dir',
+    :validations =>  {:predefined => 'unix_path'}
     
     
