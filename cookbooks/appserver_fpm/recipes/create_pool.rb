d0b3f6140d758cc08d98caf7fdebe1450714b57a
J. Gomez <alambike@gmail.com>
2015-11-11 10:16:48 +0100

    Default process_manager to ondemand

--- cookbooks/appserver_fpm/recipes/create_pool.rb
+++ cookbooks/appserver_fpm/recipes/create_pool.rb
@@ -1,5 +1,5 @@
 php5_fpm_pool node["appserver"]["fpm"]["pools"] do
-    pm                      "dynamic"
+    pm                      "ondemand"
     pm_max_requests         5000
     php_ini_admin_flags     'log_errores' => 'on'
     php_ini_admin_values    'memory_limit' => '32M'
