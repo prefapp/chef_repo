149f4c4fd642795678e5e9a2bea37d57ced6391d
Juan Iglesias <juair10@gmail.com>
2015-12-17 17:41:38 +0000

    Modificado para usar mysql

--- cookbooks/app_owncloud/recipes/default.rb
+++ cookbooks/app_owncloud/recipes/default.rb
@@ -1,2 +1,12 @@
 include_recipe "app_php::default"
-include_recipe "dbs_mysql::default"
+include_recipe "code_repo::default"
+
+dbtype = node['app']['owncloud']['db_type']
+
+if dbtype == 'mysqli'
+  include_recipe 'dbs_mysql::default'
+elsif  dbtype == 'pgsql'
+  include_recipe 'dbs_postgresql::default'
+else
+  Chef::Application.fatal!("#{dbtype} not supported yet")
+end
