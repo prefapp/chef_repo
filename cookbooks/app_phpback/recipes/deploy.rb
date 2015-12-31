2cfee800d6467d15fbfcb3e6a30bc4626ba32422
Javier Gomez <alambike@gmail.com>
2015-12-31 03:22:15 +0000

    Corrected initilization task New form of stablish node default values

--- cookbooks/app_phpback/recipes/deploy.rb
+++ cookbooks/app_phpback/recipes/deploy.rb
@@ -2,28 +2,11 @@ include_recipe "app_phpback::default"
 
 app = node['app']['phpback']
 
-args = {}
-
-%W{
-  domain
-  target_path
-  user
-  group
-  repo_url
-  repo_type
-  revision
-  db_name
-  db_user
-  db_host
-  db_password
-  db_type
-  admin_user
-  admin_password
-}.each do |attribute|
-
-  args[attribute] = app[attribute] || node["app"]["phpback"]["default_#{attribute}"]
-
+# establecemos valores por defecto
+args = Hash.new do |hash,key| 
+  hash[key] = app[key] || node["app"]["phpback"]["default_#{key}"]
 end
+
 
 # npi se fai falta
 php_ini_config = {
@@ -38,13 +21,6 @@ php_ini_config = {
 ###
 
 extra_packages = []
-#extra_packages = %w{
-#  php5-curl
-#  php5-gd 
-#  php5-intl
-#  php5-xmlrpc
-#}
-#
 
 if args['db_type'] == 'mysql'
   extra_packages << 'php5-mysqlnd'
@@ -56,7 +32,7 @@ end
 fcgi_app args["domain"] do
 
   target_path          args['target_path']
-  document_root        args["target_path"]
+  document_root        args['target_path']
   server_alias         args['alias'] if args['alias']
   action               :deploy
   owner                args['user']
@@ -90,40 +66,24 @@ template "#{args['target_path']}/application/config/database.php" do
                 
 end
 
-# extra_tasks para o arranque do container
-#if node["riyic"]["inside_container"]
-#  
-#  template "#{node['riyic']['extra_tasks_dir']}/phpback_load_db-#{app['domain']}.sh" do
-#
-#    source 'mysql_aware_tasks.sh.erb'
-#    mode '0700'
-#    owner 'root'
-#    group 'root'
-#    cookbook 'dbs_mysql'
-#    variables ({
-#      :control_file => '/root/.actualizado',
-#      :db_user => args['db_user'],
-#      :db_pass => args['db_password'],
-#      :db_host => args['db_host'],
-#      :task => "mysql -u #{args['db_user']} -p#{args['db_password']} -h #{args['db_host']} #{args['db_name']} < #{args['target_path']}/install/database_tables.sql"
-#    })
-#  end
-#end
 
+#
+# tarea de inicializacion da app
+#
 task = <<"EOF"
-php #{args['target_path']}/install/install1.php \
-'adminname=#{args['admin_name']}&adminemail=#{args['admin_email']}\
+cd #{args['target_path']}/install/ && \
+([ ! -f install1.php ] || php install1.php 'adminname=#{args['admin_name']}&adminemail=#{args['admin_email']}\
 &adminpass=#{args['admin_password']}&adminrpass=#{args['admin_password']}\
-&hostname=#{args['db_host']}&username=#{args['db_user']}&password=#{args['db_pass']}&database=#{args['db_name']}' && \
-php #{args['target_path']}/install/install2.php \
-'rpublic=#{args['rpublic']}&rprivate=#{args['rprivate']}&mainmail=#{args['mainmail']}\
+&hostname=#{args['db_host']}&username=#{args['db_user']}&password=#{args['db_password']}&database=#{args['db_name']}') && \
+([ ! -f install2.php ] || php install2.php 'rpublic=#{args['rpublic']}&rprivate=#{args['rprivate']}&mainmail=#{args['mainmail']}\
 &title=#{args['title']}&smtp-host=#{args['smtp-host']}&smtp-port=#{args['smtp-port']}\
 &smtp-user=#{args['smtp-user']}&smtp-password=#{args['smtp-password']}\
-&maxvotes=#{args['max_votes']}&max_results=#{args['max_results']}&language=#{args['language']}'
+&maxvotes=#{args['max_votes']}&max_results=#{args['max_results']}&language=#{args['language']}')
 EOF
 
 
 # executamos a task ou creamos o ficheiro para executarse no arranque
+# en funcion de si estamos instalando o server mysql ou non
 execute "installation_script" do
   command task
   only_if {node.recipe?('dbs_mysql::server')}
