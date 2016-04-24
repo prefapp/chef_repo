3f1e102744db0a4e1e292520b07ecc909c92231e
J.Gomez <alambike@gmail.com>
2016-04-24 17:26:20 +0200

    Update install recipe ssh

--- cookbooks/tools_sshd/recipes/install.rb
+++ cookbooks/tools_sshd/recipes/install.rb
@@ -7,6 +7,22 @@ template node['tools']['sshd']['config_file'] do
   mode   00755
 end
 
+# creamos a carpeta /var/run/sshd
+directory "/var/run/sshd" do
+  owner "root"
+  group "root"
+  mode 0755
+  action :create
+end
 
+
+
+# para arrancar o servicio co runit dentro dun container
+node.set['container_service']['ssh']['command'] = 
+  "/usr/sbin/sshd -D"
+
+service "ssh" do
+  action :enable
+end 
 
 
