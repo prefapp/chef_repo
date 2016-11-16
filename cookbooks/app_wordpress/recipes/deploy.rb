2ccd4c729fbca0f8b324244b8f6f538fc0110eb7
J.Gomez <alambike@gmail.com>
2016-11-16 01:40:37 +0100

    Added custom nginx vhost template to allow permalinks

--- cookbooks/app_wordpress/recipes/deploy.rb
+++ cookbooks/app_wordpress/recipes/deploy.rb
@@ -7,59 +7,61 @@ app = node['app']['wordpress']
 args = {}
 
 %W{
-    domain
-    target_path
-    user
-    group
-    repo_url
-    repo_type
-    revision
-    db_name
-    db_user
-    db_host
-    db_password
-    debug
-    debug_log
-    system_lang
+  domain
+  target_path
+  user
+  group
+  repo_url
+  repo_type
+  revision
+  db_name
+  db_user
+  db_host
+  db_password
+  debug
+  debug_log
+  system_lang
 
 }.each do |attribute|
-
-    args[attribute] = app[attribute] || node["app"]["wordpress"]["default_#{attribute}"]
+  
+  args[attribute] = app[attribute] || node["app"]["wordpress"]["default_#{attribute}"]
 
 end
 
 php_ini_config = {
-    'upload_max_filesize' => '50M',
-    'post_max_size' => '55M',
-    
-    'opcache.enable' => 1,
-    'opcache.memory_consumption' => 128,
-    'opcache.max_accelerated_files' =>  4000,
-    'opcache.revalidate_freq' => 60,
+  'upload_max_filesize' => '50M',
+  'post_max_size' => '55M',
+  
+  'opcache.enable' => 1,
+  'opcache.memory_consumption' => 128,
+  'opcache.max_accelerated_files' =>  4000,
+  'opcache.revalidate_freq' => 60,
 }
 
 
 # instalamos a aplicacion e configuramos os servicios necesarios
 fcgi_app args["domain"] do
 
-    target_path          args['target_path']
-    document_root        args["target_path"]
-    server_alias         args['alias'] if args['alias']
-    action               :deploy
-    owner                args['user']
-    group                args['group']
-    timeout              "600"
-
-    repo_url             args["repo_url"]
-    repo_type            args["repo_type"]
-    revision             args["revision"]
-    purge_target_path    'yes'
-    extra_packages       ["php5-curl", "php5-mysqlnd"]
-
-    php_ini_admin_values (php_ini_config)
-
-    notifies             :restart, 'service[nginx]', :delayed
-    notifies             :restart, 'service[php5-fpm]', :delayed
+  target_path          args['target_path']
+  document_root        args["target_path"]
+  server_alias         args['alias'] if args['alias']
+  action               :deploy
+  owner                args['user']
+  group                args['group']
+  timeout              "600"
+  
+  repo_url             args["repo_url"]
+  repo_type            args["repo_type"]
+  revision             args["revision"]
+  purge_target_path    'yes'
+  extra_packages       ["php5-curl", "php5-mysqlnd"]
+  
+  php_ini_admin_values (php_ini_config)
+  
+  cookbook	           'app_moodle'
+  frontend_template    'nginx_moodle.erb'  
+  notifies             :restart, 'service[nginx]', :delayed
+  notifies             :restart, 'service[php5-fpm]', :delayed
 
 end
 
@@ -81,10 +83,10 @@ args['debug_log'] = (app['enable_debug_log'] =~ /^y|s/i)? true : false
 # creamos o ficheiro de configuracion
 template "#{args['target_path']}/wp-config.php" do
 
-    source      'wp-config.php.erb'
-    cookbook    "app_wordpress" 
-    user        args["user"]
-    group       args["group"]
-    variables   ({:app => args})
-                
+  source      'wp-config.php.erb'
+  cookbook    "app_wordpress" 
+  user        args["user"]
+  group       args["group"]
+  variables   ({:app => args})
+                  
 end
