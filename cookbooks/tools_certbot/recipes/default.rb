3768451be22b344e2520874c77f514cc5f39d1dd
J.Gomez <alambike@gmail.com>
2017-05-29 14:31:49 +0200

    Quitamos comillas das variables de entorno, e agregamos '-n' a certbot para que non pregunte

--- cookbooks/tools_certbot/recipes/default.rb
+++ cookbooks/tools_certbot/recipes/default.rb
@@ -18,15 +18,15 @@ file '/root/renovar_cert.sh' do
 #!/bin/bash
 
 sleep 6 && \
-certbot certonly --webroot --webroot-path /usr/share/nginx/html \
---cert-name $LETSENCRYPT_PRINCIPAL \
---domains $LETSENCRYPT_DOMAINS \
---text --agree-tos --email $LETSENCRYPT_EMAIL \
+certbot certonly -n --webroot --webroot-path /usr/share/nginx/html \
+--cert-name ${LETSENCRYPT_PRINCIPAL//\"/} \
+--domains ${LETSENCRYPT_DOMAINS//\"/} \
+--text --agree-tos --email ${LETSENCRYPT_EMAIL//\"/} \
 --server https://acme-v01.api.letsencrypt.org/directory \
 --rsa-key-size 4096 --verbose --keep \
 --standalone-supported-challenges http-01 && \
 rm -rf /etc/letsencrypt/live/default && \
-ln -sf /etc/letsencrypt/live/$LETSENCRYPT_PRINCIPAL /etc/letsencrypt/live/default && \
+ln -sf /etc/letsencrypt/live/${LETSENCRYPT_PRINCIPAL//\"/} /etc/letsencrypt/live/default && \
 sv restart nginx
   EOF
 
