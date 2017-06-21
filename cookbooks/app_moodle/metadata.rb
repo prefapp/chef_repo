d8d05a2f850a31ad1b0e4348a47c1ef729992d65
J.Gomez <alambike@gmail.com>
2017-06-21 14:59:44 +0200

    Corregido metadata

--- cookbooks/app_moodle/metadata.rb
+++ cookbooks/app_moodle/metadata.rb
@@ -137,7 +137,7 @@ attribute "app/moodle/admin_password",
     :validations => {predefined: "password"}
 
 
-attribute "app/moodle/max_upload_size"
+attribute "app/moodle/max_upload_size",
     :display_name => 'Maximum upload size',
     :description => 'Max size of uploadable file in MB',
     :default => '120',
