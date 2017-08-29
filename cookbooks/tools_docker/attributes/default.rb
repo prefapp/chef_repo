4f6475ef4625b0e22607ca6ee97d2c251a54dd6e
J.Gomez <alambike@gmail.com>
2017-08-29 18:24:46 +0200

    Actualizada version docker-engine

--- cookbooks/tools_docker/attributes/default.rb
+++ cookbooks/tools_docker/attributes/default.rb
@@ -1,4 +1,4 @@
 default['tools']['docker']['compose_version']  = "1.15.0"
 default['tools']['docker']['machine_version']  = "v0.6.0"
-default['tools']['docker']['engine_version']  = "17.05.0-ce"
+default['tools']['docker']['engine_version']  = "17.06.1-ce"
 
