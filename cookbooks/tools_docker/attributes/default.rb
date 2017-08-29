01a439dd5d72e2b84c5494e3f62fbe97f96d8287
J.Gomez <alambike@gmail.com>
2017-08-29 18:24:34 +0200

    Actualizada version de docker-compose

--- cookbooks/tools_docker/attributes/default.rb
+++ cookbooks/tools_docker/attributes/default.rb
@@ -1,4 +1,4 @@
-default['tools']['docker']['compose_version']  = "1.6.2"
+default['tools']['docker']['compose_version']  = "1.15.0"
 default['tools']['docker']['machine_version']  = "v0.6.0"
 default['tools']['docker']['engine_version']  = "17.05.0-ce"
 
