543631b4bb95741042d382b75706c6338f0483f9
J.Gomez <alambike@gmail.com>
2018-01-02 14:10:16 +0100

    Actualizadas versiones de nginx y passenger a actuales estables

--- cookbooks/appserver_nginx/attributes/default.rb
+++ cookbooks/appserver_nginx/attributes/default.rb
@@ -7,11 +7,11 @@ default['appserver']['nginx']['modules'] = [
 #default['appserver']['nginx']['checksum'] = '1045ac4987a396e2fa5d0011daf8987b612dd2f05181b67507da68cbe7d765c2'
 
 
-default['appserver']['nginx']['version'] = '1.12.0'
-default['appserver']['nginx']['checksum'] = 'b4222e26fdb620a8d3c3a3a8b955e08b713672e1bc5198d1e4f462308a795b30'
+default['appserver']['nginx']['version'] = '1.12.2'
+default['appserver']['nginx']['checksum'] = '305f379da1d5fb5aefa79e45c829852ca6983c7cd2a79328f8e084a324cf0416'
 
 # directorio de instalacion (por defecto)
 default['appserver']['nginx']['install_dir'] = '/opt/nginx'
 
 # default passenger version
-default['appserver']['nginx']['passenger']['version'] = '5.1.4'
+default['appserver']['nginx']['passenger']['version'] = '5.1.12'
